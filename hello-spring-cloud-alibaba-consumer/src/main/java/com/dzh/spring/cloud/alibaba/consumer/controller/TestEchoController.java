cf8a492d3ff24bd54add41c95ddd4363156340eb
ddingzuoheng <dingzuoheng@126.com>
2020-02-28 10:20:52 +0800

    变更使用nacos配置中心，并加入@RefreshScope注解，实时更新配置变更

--- hello-spring-cloud-alibaba-consumer/src/main/java/com/dzh/spring/cloud/alibaba/consumer/controller/TestEchoController.java
+++ hello-spring-cloud-alibaba-consumer/src/main/java/com/dzh/spring/cloud/alibaba/consumer/controller/TestEchoController.java
@@ -2,6 +2,8 @@ package com.dzh.spring.cloud.alibaba.consumer.controller;
 
 import com.dzh.spring.cloud.alibaba.consumer.service.EchoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,19 +15,28 @@ import org.springframework.web.bind.annotation.RestController;
  * @Date 2020/2/25
  * @Version 1.0
  **/
+@RefreshScope
 @RestController
 public class TestEchoController {
 
     @Autowired
     private EchoService echoService;
 
+    @Value("${user.name}")
+    private String username;
+
+    @GetMapping(value = "/config")
+    public String config() {
+        return echoService.echo(username);
+    }
+
     @GetMapping("feign/echo/{str}")
-    public String echo(@PathVariable String str){
-        return "调用feign后得到的结果: "+echoService.echo(str);
+    public String echo(@PathVariable String str) {
+        return "调用feign后得到的结果: " + echoService.echo(str);
     }
 
     @GetMapping("/feign/lb")
-    public  String lb() {
-        return  echoService.lb();
+    public String lb() {
+        return echoService.lb();
     }
 }
